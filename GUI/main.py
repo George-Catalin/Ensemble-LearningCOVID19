f8fce5592ef6bfebf8d6642c2d3ea8aa3bdd1799
George <45779577+George-Catalin@users.noreply.github.com>
2022-05-12 20:35:08 +0100

    Add files via upload

---
+++ GUI/main.py
@@ -0,0 +1,46 @@
+from flask import Flask , render_template , request, url_for
+from flask_ngrok import run_with_ngrok 
+import utils 
+
+from PIL import Image
+import gradcam
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def indexpage():
+    return render_template("index.html") 
+
+@app.route("/submit" , methods = ["POST"])
+def submitdata():
+    if request.method == "POST":
+        f = request.files["userfile"]
+        print(f , "fffff")
+        
+        
+        path = "C:\\Users\\musel\\OneDrive\\Desktop\\FYPGeorgeMuselin\\GUI\\static\\{}".format(f.filename)
+        f.save(path)
+        print(path)
+        l , p   = utils.predict(path)   #("COVID",[0.98,0.02])  
+        grad_img = gradcam.gradcam_image_process(path , l , p )
+     
+        img = Image.fromarray(grad_img)
+             
+        print(img) 
+        img.save("C:\\Users\\musel\\OneDrive\\Desktop\\FYPGeorgeMuselin\\GUI\\static\\inception_img.jpeg")  
+        my_ans = {
+             "label" : l , 
+             "image" :  "./static/{}".format(f.filename),
+            
+             "imgincep" :   "C:\\Users\\musel\\OneDrive\\Desktop\\FYPGeorgeMuselin\\GUI\\static\\inception_img.jpeg",
+            "imagedesnet" :  "./static/gradcam{}".format(utils.models_name[1] + "img"),
+            "imageresnet" :  "./static/gradcam{}".format(utils.models_name[2] + "img"),
+            "neg" : p[1] , 
+            "pos" : p[0]
+             }
+       
+        print("Analysis done")   
+    return render_template("index.html" ,  my_res = my_ans )
+        
+if __name__ == "__main__":
+    app.run()
